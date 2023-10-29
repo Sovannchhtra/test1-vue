10549f9541191cace14247d790344174e4ea6418
macOS <macos@MacBook-Pro.local>
2023-10-29 18:03:16 +0700

    Change file

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -21,6 +21,7 @@ import { ref } from 'vue';
                     <div class="w-4rem h-4rem bg-primary font-bold flex align-items-center justify-content-center p-4 border-round mr-3">2</div>
                     <div class="w-4rem h-4rem bg-primary font-bold flex align-items-center justify-content-center p-4 border-round">3</div>
                </div>
+               <h1>Hello Crush</h1>
      </div>
      
 </template>
